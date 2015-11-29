d36d763404a668632161fd8539068346f47e79fe
root <root@debian>
2015-11-30 00:26:31 +0100

    Rajoute des 10 derniers resultats de recherche, img avec le login, btn recherche avancé

--- bigdatass/src/SearchTweet/Bundle/Controller/DefaultController.php
+++ bigdatass/src/SearchTweet/Bundle/Controller/DefaultController.php
@@ -16,7 +16,11 @@ class DefaultController extends Controller
 {
   public function indexAction()
   {
-    return $this->render('SearchTweetBundle:Default:index.html.twig');
+    $m = new MongoClient();
+    $db = $m->selectDB('bigdatass');
+    $collection = new MongoCollection($db, 'recent');
+    $data = $collection->find();
+    return $this->render('SearchTweetBundle:Default:index.html.twig', array("data" => $data));
   }
 
   public function searchAction()
@@ -50,6 +54,7 @@ class DefaultController extends Controller
 	    $tab[$i]["text"] = $doc["text"];
 	    $tab[$i]["created_at"] = $doc["created_at"];
 	    $tab[$i]["username"] = $doc["user"]["screen_name"];
+	    $tab[$i]["username_img"] = $doc["user"]["profile_image_url"];
 	    $tab[$i]["id_str"] = $doc["id_str"];
 	    $rand = rand(1, 3);
 	    $tab[$i]["SentimentA"] = ($rand == 1 ? "negative" : ($rand == 3 ? "positive" : "neutral"));
@@ -64,7 +69,9 @@ class DefaultController extends Controller
       $tab[$i]["id_str"] = "Null";
       $tab[$i]["SentimentA"] = "Null";
     }
-    return $this->render('SearchTweetBundle:Default:show.html.twig', array("data" => $tab));
+    $collection = new MongoCollection($db, 'recent');
+    $collection->insert(array('search' => $keyword, 'stats' => array_count_values(array_map(function($foo){return $foo['SentimentA'];}, $tab))));
+    return $this->render('SearchTweetBundle:Default:show.html.twig', array("data" => $tab, "search" => $keyword));
   }
 
   public function searchadvancedAction()
@@ -72,34 +79,35 @@ class DefaultController extends Controller
     $tab = array(array());
     $i = 0;
     $query = "";
+    $search = "";
     $today = date("Y-m-d", strtotime('-1 day'));
     if (isset($_POST["form"]["allwordAnd"]) && !empty($_POST["form"]["allwordAnd"])) {
       $keyword = addslashes($_POST["form"]["allwordAnd"]);
       $query .= $keyword . "%20";
+      $search = strlen($search) ? $search . ', ' .$keyword : $keyword;
     }
     if (isset($_POST["form"]["allTag"]) && !empty($_POST["form"]["allTag"])) {
       $keyword = addslashes($_POST["form"]["allTag"]);
       $query .= $keyword . "%20";
+      $search = strlen($search) ? $search . ', ' .$keyword : $keyword;
     }
     if (isset($_POST["form"]["allwordOr"]) && !empty($_POST["form"]["allwordOr"])) {
       $keyword = addslashes($_POST["form"]["allwordOr"]);
       $keyword = preg_replace("/ /", "%20OR%20", $keyword);
       $query .= $keyword . "%20";
+      $search = strlen($search) ? $search . ', ' .$keyword : $keyword;
     }
     if (isset($_POST["form"]["sentence"]) && !empty($_POST["form"]["sentence"])) {
       $keyword = addslashes($_POST["form"]["sentence"]);
       $query .= '"' . $keyword . '"' . "%20";
+      $search = strlen($search) ? $search . ', ' .$keyword : $keyword;
     }
     if (isset($_POST["form"]["allwordNot"]) && !empty($_POST["form"]["allwordNot"])) {
       $keyword = addslashes($_POST["form"]["allwordNot"]);
       $keyword = preg_replace("/ /", "%20-", $keyword);
       $query .= "-" . $keyword . "%20";
+      $search = strlen($search) ? $search . ', ' .$keyword : $keyword;
     }
-    //    if (isset($_POST["form"]["language"]) && !empty($_POST["form"]["language"])) {
-    //      $keyword = addslashes($_POST["form"]["language"]);
-    //      $keyword = preg_replace("/ /", "%20OR%20%40", $keyword);
-    //      $query .= "lang%3A" . $keyword . "%20";
-    //    }
     if (isset($keyword) && !empty($keyword)) {
       $m = new MongoClient();
       $db = $m->selectDB('bigdatass');
@@ -125,6 +133,7 @@ class DefaultController extends Controller
 	  $tab[$i]["text"] = $doc["text"];
 	  $tab[$i]["created_at"] = $doc["created_at"];
 	  $tab[$i]["username"] = $doc["user"]["screen_name"];
+	  $tab[$i]["username_img"] = $doc["user"]["profile_image_url"];
 	  $tab[$i]["id_str"] = $doc["id_str"];
 	  $rand = rand(1, 3);   // Note à celui qui comment la ligne, il n'y a que 1000 requêtes/j, elles sont precieuses
 	  $tab[$i]["SentimentA"] = ($rand == 1 ? "negative" : ($rand == 3 ? "positive" : "neutral"));   // Note à celui qui comment la ligne, il n'y a que 1000 requêtes/j, elles sont precieuses
@@ -139,7 +148,9 @@ class DefaultController extends Controller
       $tab[$i]["id_str"] = 'Null';
       $tab[$i]["SentimentA"] = 'Null';
     }
-    return $this->render('SearchTweetBundle:Default:show_advanced.html.twig', array("data" => $tab));
+    $collection = new MongoCollection($db, 'recent');
+    $collection->insert(array('search' => $keyword, 'stats' => array_count_values(array_map(function($foo){return $foo['SentimentA'];}, $tab))));
+    return $this->render('SearchTweetBundle:Default:show_advanced.html.twig', array("data" => $tab, "search" => $search));
   }
 
   public function newAction(Request $request)
@@ -176,37 +187,6 @@ class DefaultController extends Controller
 						    ),
 				    'required' => false,
 				    ))
-      //      ->add('language', 'language', array(
-      //					  'placeholder' => 'Choose an option',
-      //					  'attr' => array(
-      //							  'class' => 'form-group form-control',
-      //							  ),
-      //					  'required' => false,
-      //					  ))
-      //      ->add('usernameFrom', 'text', array(
-      //					  'attr' => array(
-      //							  'class' => 'form-group form-control',
-      //							  ),
-      //					  'required' => false,
-      //					  ))
-      //      ->add('usernameTo', 'text', array(
-      //					'attr' => array(
-      //							'class' => 'form-group form-control',
-      //							),
-      //					'required' => false,
-      //					))
-      //      ->add('usernameQuoted', 'text', array(
-      //					    'attr' => array(
-      //							    'class' => 'form-group form-control',
-      //							    ),
-      //					    'required' => false,
-      //					    ))
-      //      ->add('generation', 'text', array(
-      //					'attr' => array(
-      //							'class' => 'form-group form-control',
-      //							),
-      //					'required' => false,
-      //					))
       ->add('save', 'submit', array('label' => 'Create Query',
 				    'attr' => array(
 						    'class' => 'btn btn-default',
@@ -216,19 +196,4 @@ class DefaultController extends Controller
 								   'form' => $form->createView(),
 								   ));
   }
-
-  //  public function showAction()
-  //{
-  //$m = new MongoClient();
-  //$db = $m->selectDB('bigdatass');
-  //$collection = new MongoCollection($db, 'tweets');
-  //$tab = [];
-  //$cursor = $collection->find();
-  //foreach ($cursor as $doc)
-  //  {
-  //	$tab["text"] = $doc["text"];
-  //	$tab["username"] = $doc["user"]["screen_name"];
-  //  }
-  //return $this->render('TestBundle:Default:show.html.twig', array("data", $tab));
-  //}
 }
